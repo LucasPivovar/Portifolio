18c969a4c96f33610c8a87395d199180333c1a04
LucasPivovar <lucascaetano956@gmail.com>
2025-05-18 21:01:18 -0300

    arrumando deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   // Configuração opcional para melhorar a performance do build
   productionSourceMap: false,
+  lintOnSave: false,
   // Se você tiver problemas com CORS durante desenvolvimento
   devServer: {
     proxy: {
